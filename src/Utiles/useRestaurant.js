569833b1c8d0152eba93d0c59681609de87fdfcd
Palak Garg <palakgarg296.com>
2023-04-13 16:12:28 +0530

    resolved cors issue

--- src/Utiles/useRestaurant.js
+++ src/Utiles/useRestaurant.js
@@ -4,7 +4,7 @@ import { useState,useEffect } from "react";
 
       async function getRestaurants() {
         // handle the error using try... catch
-         const data = await fetch("https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
           const json = await data.json();
           // updated state variable restaurants with Swiggy API data
            return json?.data?.cards[2]?.data?.data?.cards;
